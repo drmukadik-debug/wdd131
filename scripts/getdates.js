301248eee9e0a2d059b5957ea837892e7e61f085
drmukadik-debug <drmukadik@gmail.com>
2026-07-11 21:51:14 +0200

    added changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,3 @@
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
+document.getElementById("currentyear").textContent = new Date().getFullYear(); 
 document.getElementById("lastModified").textContent = 
 "Last Modification: " + document.lastModified;
